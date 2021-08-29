f3bc661d3ed43a68f74cb19c1eb8ba339270ecf9
sagarka <katwalsagar07@gmail.com>
2021-08-29 15:29:44 +0545

    first commit

---
+++ first.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
+<html>
+<body>
+<h1>my second PHP_ROUND_HALF_DOWN
+<?php
+echo"Hello World!";
+?>
+
+
+<html>
+<body>
+
+<h2>my third php_ini_loaded_file
+<?phpecho"hello world!";
+echo"Hello World";
+?>
+
+</DOCTYPE html>
+
+<htmlentities>
+<bzopen>
+<html_entity_decode mb_convert_encoding>
+<body>
+<highlight_string mysecond PHP_ROUND_HALF_DOWN<?
+php_ini_loaded_fileeche 
+<h1>my stream_socket_sendto
+<?php
+echo"Hello World!";
+?>
+
+<html>
+<body>
+
+<h6>my fourth php_logo_guid
+<?php "hello world"
+?<
+
+<htmlspecialchars>
+<bzdecompress>
+<body>
+<highlight_file mb_decode_numericentity <?
+php_strip_whitespace
+ 
